40131382d1e4cbd1f4bd1631b07dda5bd9cd4fb5
Yuri D'Elia <yuri.delia@eurac.edu>
2009-07-10 17:55:10 +0200

    Undo changes to config.php.

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -1,10 +1,10 @@
 <?php
 // user config
 $phpExt = ".php"; // you can use "" if you enable apache's MultiViews option
-$masterPath = "https://abz03gm.eurac.edu/~ydelia/dl/htdocs/";
+$masterPath = "http://dl.example.com/";
 $dbHandler = "db4"; // change to db3/db2/dbm depending on your php version
 $maxSize = ini_get('upload_max_filesize');
 $fromAddr = "ticket service <nobody@example.com>";
 $masterPass = "change me";
-$spoolDir = "/home/ydelia/public_html/.dl/";
+$spoolDir = "/var/spool/dl/";
 ?>
