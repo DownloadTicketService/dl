c8fa183cc99f253fcc04e1f9402fe3cfe62a03bc
Yuri D'Elia <yuri.delia@eurac.edu>
2009-07-13 16:35:32 +0200

    Remove $masterPass from config, no longer used.

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -5,6 +5,5 @@ $masterPath = "http://dl.example.com/";
 $dbHandler = "db4"; // change to db3/db2/dbm depending on your php version
 $maxSize = ini_get('upload_max_filesize');
 $fromAddr = "ticket service <nobody@example.com>";
-$masterPass = "change me";
 $spoolDir = "/var/spool/dl/";
 ?>
