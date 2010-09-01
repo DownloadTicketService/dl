483294b7b37c7b7a294cade175aee0ee67bc6529
Yuri D'Elia <yuri.delia@eurac.edu>
2010-09-01 11:53:42 +0200

    Fix selectAll() without arguments.

--- htdocs/static/dl.js
+++ htdocs/static/dl.js
@@ -96,7 +96,7 @@ function toggleAdvanced(set)
 
 function selectAll(v)
 {
-  if(v === null) v = true;
+  if(v === undefined) v = true;
   $('input:checkbox', document.forms[0]).attr('checked', v);
 }
 
