5493a9127636cf3af94568caf86aca7722659229
Yuri D'Elia <yuri.delia@eurac.edu>
2011-03-28 18:34:21 +0200

    Improve dbupgrade.php to handle upgrade from any earlier version.

--- htdocs/include/scripts/dbupgrade.php
+++ htdocs/include/scripts/dbupgrade.php
@@ -8,102 +8,134 @@ if(!isset($argc)) die("not running from the command line\n");
 require_once("../confwrap.php");
 require_once("../admfuncs.php");
 
-// initialize the old 0.3 dbs
-if(!isset($dbHandler)) $dbHandler = "db4";
-$tDbPath = $spoolDir . "/data.db";
-$uDbPath = $spoolDir . "/user.db";
-$tDb = dba_popen($tDbPath, "r", $dbHandler) or die();
-$uDb = dba_popen($uDbPath, "r", $dbHandler) or die();
-
-// initialize the new db connection
+// initialize the db connection
 $db = new PDO($dsn);
 $db->exec('PRAGMA foreign_keys = ON');
 
-// convert user information
-echo "converting users ...\n";
+// fetch current db release
+$sql = "SELECT value FROM config WHERE name = 'version'";
+$version = @$db->query($sql)->fetchColumn();
 
-$userId = $db->query("SELECT id FROM role WHERE name = 'user'")->fetchColumn();
-$adminId = $db->query("SELECT id FROM role WHERE name = 'admin'")->fetchColumn();
-
-for($key = dba_firstkey($uDb); $key; $key = dba_nextkey($uDb))
+$tDbPath = $spoolDir . "/data.db";
+$uDbPath = $spoolDir . "/user.db";
+if(!$version && file_exists($tDbPath) && file_exists($uDbPath))
 {
-  $DATA = dba_fetch($key, $uDb);
-  if($DATA === false) continue;
-  $DATA = unserialize($DATA);
+  echo "upgrading <0.3 => 0.3 ...\n";
 
-  echo " ... $key\n";
+  // initialize the old 0.3 dbs
+  if(!isset($dbHandler)) $dbHandler = "db4";
+  $tDb = dba_popen($tDbPath, "r", $dbHandler) or die();
+  $uDb = dba_popen($uDbPath, "r", $dbHandler) or die();
 
-  // prepare the SQL
-  $sql = "INSERT INTO user (name, pass_md5, role_id) VALUES (";
-  $sql .= $db->quote($key);
-  $sql .= ", " . (empty($DATA["pass"])?
-      'NULL': $db->quote($DATA["pass"]));
-  $sql .= ", " . ($DATA["admin"]? $adminId: $userId);
-  $sql .= ")";
+  // convert user information
+  echo "converting users ...\n";
 
-  $db->exec($sql);
+  $userId = $db->query("SELECT id FROM role WHERE name = 'user'")->fetchColumn();
+  $adminId = $db->query("SELECT id FROM role WHERE name = 'admin'")->fetchColumn();
+
+  for($key = dba_firstkey($uDb); $key; $key = dba_nextkey($uDb))
+  {
+    $DATA = dba_fetch($key, $uDb);
+    if($DATA === false) continue;
+    $DATA = unserialize($DATA);
+
+    echo " ... $key\n";
+
+    // prepare the SQL
+    $sql = "INSERT INTO user (name, pass_md5, role_id) VALUES (";
+    $sql .= $db->quote($key);
+    $sql .= ", " . (empty($DATA["pass"])?
+        'NULL': $db->quote($DATA["pass"]));
+    $sql .= ", " . ($DATA["admin"]? $adminId: $userId);
+    $sql .= ")";
+
+    $db->exec($sql);
+  }
+
+  $xUsers = array();
+
+  for($key = dba_firstkey($tDb); $key; $key = dba_nextkey($tDb))
+  {
+    $DATA = dba_fetch($key, $tDb);
+    if($DATA === false) continue;
+    $DATA = unserialize($DATA);
+
+    $xUsers[$DATA["user"]] = true;
+  }
+
+  foreach($xUsers as $key => $value)
+  {
+    echo " ... $key\n";
+
+    // prepare the SQL
+    $sql = "INSERT INTO user (name, pass_md5, role_id) VALUES (";
+    $sql .= $db->quote($key);
+    $sql .= ", NULL";
+    $sql .= ", $userId";
+    $sql .= ")";
+
+    $db->exec($sql);
+  }
+
+  echo "done\n";
+
+
+  // convert ticket information
+  echo "converting tickets ...\n";
+
+  for($key = dba_firstkey($tDb); $key; $key = dba_nextkey($tDb))
+  {
+    $DATA = dba_fetch($key, $tDb);
+    if($DATA === false) continue;
+    $DATA = unserialize($DATA);
+
+    echo " ... $key\n";
+
+    // prepare the SQL
+    $sql = "INSERT INTO ticket (id, user_id, name, path, size, cmt, time"
+      . ", downloads, last_stamp, last_time, expire, expire_last, expire_dln"
+      . ", notify_email) VALUES (";
+    $sql .= $db->quote($key);
+    $sql .= ", (SELECT id FROM user WHERE name = " . $db->quote($DATA["user"]) . ")";
+    $sql .= ", " . $db->quote($DATA["name"]);
+    $sql .= ", " . $db->quote($DATA["path"]);
+    $sql .= ", " . $DATA["size"];
+    $sql .= ", " . (empty($DATA["cmt"])? 'NULL': $db->quote($DATA["cmt"]));
+    $sql .= ", " . $DATA["time"];
+    $sql .= ", " . $DATA["downloads"];
+    $sql .= ", " . ($DATA["lastTime"] == 0? 'NULL': $DATA["lastTime"]);
+    $sql .= ", " . ($DATA["expireLast"] == 0? 'NULL': $DATA["expireLast"]);
+    $sql .= ", " . ($DATA["expire"] == 0? 'NULL': $DATA["expire"]);
+    $sql .= ", " . ($DATA["expireLast"] == 0 || $DATA["lastTime"] == 0? 'NULL': $DATA["lastTime"] + $DATA["expireLast"]);
+    $sql .= ", " . ($DATA["expireDln"] == 0? 'NULL': $DATA["expireDln"]);
+    $sql .= ", " . (empty($DATA["email"])? 'NULL': $db->quote($DATA["email"]));
+    $sql .= ")";
+
+    $db->exec($sql);
+  }
+
+  $version = "0.3";
 }
 
-$xUsers = array();
-
-for($key = dba_firstkey($tDb); $key; $key = dba_nextkey($tDb))
+if(!$version || $version == "0.3")
 {
-  $DATA = dba_fetch($key, $tDb);
-  if($DATA === false) continue;
-  $DATA = unserialize($DATA);
+  echo "upgrading 0.3 => 0.4 ...\n";
 
-  $xUsers[$DATA["user"]] = true;
+  $db->exec("CREATE TABLE config (name VARCHAR PRIMARY KEY, value VARCHAR)");
+  $db->exec("INSERT INTO config VALUES('version', '0.4')");
+
+  $version = "0.4";
 }
 
-foreach($xUsers as $key => $value)
+if($version == "0.4"
+|| $version == "0.5"
+|| $version == "0.6"
+|| $version == "0.7"
+|| $version == "0.8")
 {
-  echo " ... $key\n";
-
-  // prepare the SQL
-  $sql = "INSERT INTO user (name, pass_md5, role_id) VALUES (";
-  $sql .= $db->quote($key);
-  $sql .= ", NULL";
-  $sql .= ", $userId";
-  $sql .= ")";
-
-  $db->exec($sql);
-}
-
-echo "done\n";
-
-
-// convert ticket information
-echo "converting tickets ...\n";
-
-for($key = dba_firstkey($tDb); $key; $key = dba_nextkey($tDb))
-{
-  $DATA = dba_fetch($key, $tDb);
-  if($DATA === false) continue;
-  $DATA = unserialize($DATA);
-
-  echo " ... $key\n";
-
-  // prepare the SQL
-  $sql = "INSERT INTO ticket (id, user_id, name, path, size, cmt, time"
-    . ", downloads, last_stamp, last_time, expire, expire_last, expire_dln"
-    . ", notify_email) VALUES (";
-  $sql .= $db->quote($key);
-  $sql .= ", (SELECT id FROM user WHERE name = " . $db->quote($DATA["user"]) . ")";
-  $sql .= ", " . $db->quote($DATA["name"]);
-  $sql .= ", " . $db->quote($DATA["path"]);
-  $sql .= ", " . $DATA["size"];
-  $sql .= ", " . (empty($DATA["cmt"])? 'NULL': $db->quote($DATA["cmt"]));
-  $sql .= ", " . $DATA["time"];
-  $sql .= ", " . $DATA["downloads"];
-  $sql .= ", " . ($DATA["lastTime"] == 0? 'NULL': $DATA["lastTime"]);
-  $sql .= ", " . ($DATA["expireLast"] == 0? 'NULL': $DATA["expireLast"]);
-  $sql .= ", " . ($DATA["expire"] == 0? 'NULL': $DATA["expire"]);
-  $sql .= ", " . ($DATA["expireLast"] == 0 || $DATA["lastTime"] == 0? 'NULL': $DATA["lastTime"] + $DATA["expireLast"]);
-  $sql .= ", " . ($DATA["expireDln"] == 0? 'NULL': $DATA["expireDln"]);
-  $sql .= ", " . (empty($DATA["email"])? 'NULL': $db->quote($DATA["email"]));
-  $sql .= ")";
-
-  $db->exec($sql);
+  echo "upgrading 0.4 => 0.9 ...\n";
+  $db->exec("UPDATE config SET value = '0.9' WHERE name = 'version'");
+  $version = "0.9";
 }
 
 echo "done\n";
