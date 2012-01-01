9271512434caa1b4e0699ce99b4a2264307f2143
Yuri D'Elia <wavexx@thregr.org>
2012-01-01 19:20:02 +0100

    Prettify the command-line client a bit, write some documentation.

--- client/dl-cli.py
+++ client/dl-cli.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import configobj
+import validate
 import pycurl
 import httplib
 import StringIO
 import json
+import argparse
 import os.path
 import sys
+
+DL_VERSION = "0.10"
+DL_AGENT = "dl-cli/" + DL_VERSION
 
 
 class UploadError(Exception):
@@ -15,11 +20,6 @@ class UploadError(Exception):
         return self.value
 
 
-def progress(download_t, download_d, upload_t, upload_d):
-    if upload_d > 0:
-        print >> sys.stderr, "uploading: {:-7.3f}%\r".format(upload_d * 100 / upload_t),
-
-
 def newticket(file, params):
     s = StringIO.StringIO()
     c = pycurl.Curl()
@@ -27,14 +27,19 @@ def newticket(file, params):
     c.setopt(c.WRITEFUNCTION, s.write)
 
     if sys.stdout.isatty():
+        def progress(download_t, download_d, upload_t, upload_d):
+            if upload_d > 0:
+                print >> sys.stderr, "uploading: {:-7.3f}%\r".format(upload_d * 100 / upload_t),
         c.setopt(c.NOPROGRESS, False)
         c.setopt(c.PROGRESSFUNCTION, progress)
 
+    c.setopt(c.HTTPAUTH, c.HTTPAUTH_BASIC)
+    c.setopt(c.USERPWD, params['user'] + ':' + params['pass'])
     c.setopt(c.HTTPPOST,
              [("file", (c.FORM_FILE, file)),
               ("auth", json.dumps({"user": params['user'], "pass": params['pass']})),
               ("msg", json.dumps({}))])
-    c.setopt(c.HTTPHEADER, ['Expect:', 'User-agent: dl-cli'])
+    c.setopt(c.HTTPHEADER, ['Expect:', 'User-agent: ' + DL_AGENT])
     if not params['verify']:
         c.setopt(c.SSL_VERIFYPEER, False)
 
@@ -58,20 +63,34 @@ def newticket(file, params):
             error = ret['error']
         raise UploadError("Service error: " + error)
     if ret is None:
-        raise UploadError("Server error: cannot decode output JSON");
+        raise UploadError("Service error: cannot decode output JSON");
 
     c.close()
     return ret
 
 
-if __name__ == "__main__":
-    cfgpath = os.path.expanduser("~/.dl.rc")
+def die(descr, code=1):
+    print >> sys.stderr, sys.argv[0] + ": " + descr
+    exit(code)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Upload a file to DL", epilog=DL_AGENT)
+    parser.add_argument('-r', metavar="file", dest="rc", default="~/.dl.rc", help="Use alternate RC file")
+    parser.add_argument('file', help="File to upload")
+    args = parser.parse_args()
+
+    cfgpath = os.path.expanduser(args.rc)
     cfg = configobj.ConfigObj(cfgpath)
-    if 'verify' not in cfg:
-        cfg['verify'] = False
+    v = validate.Validator()
+    cfg['verify'] = v.check('boolean', cfg.get('verify', True))
+
     try:
-        answ = newticket(sys.argv[1], cfg)
-        print answ['url']
-    except Exception as e:
-        print >> sys.stderr, sys.argv[0] + ": " + str(e)
-        exit(1)
+        answ = newticket(args.file, cfg)
+        print(answ['url'])
+    except UploadError as e:
+        die(str(e))
+
+
+if __name__ == "__main__":
+    main()
