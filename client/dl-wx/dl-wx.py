f80a9bc9b2f594fc03831840e8425deee423e0e4
Yuri D'Elia <yuri.delia@eurac.edu>
2012-01-07 20:42:46 +0100

    Implement "New Ticket" and ticket parameters in dl-wx.

--- client/dl-wx/dl-wx.py
+++ client/dl-wx/dl-wx.py
@@ -3,6 +3,7 @@ import configobj
 import validate
 import argparse
 import os.path
+import copy
 import wx
 import wx.xrc as xrc
 from dl import *
@@ -42,13 +43,12 @@ class Prefs(wx.Dialog):
         self.service = service
         self.change_fn = change_fn
         self.xrc = xrc.XmlResource('preferences.xrc')
-        self.prefs = self.xrc.LoadDialog(None, 'preferences')
-        self.url = xrc.XRCCTRL(self.prefs, 'url')
-        self.username = xrc.XRCCTRL(self.prefs, 'username')
-        self.password = xrc.XRCCTRL(self.prefs, 'password')
-        self.verify = xrc.XRCCTRL(self.prefs, 'verify')
-        self.PostCreate(self.prefs)
+        self.PostCreate(self.xrc.LoadDialog(None, 'preferences'))
         self.Bind(wx.EVT_CLOSE, self.on_close)
+        self.url = xrc.XRCCTRL(self, 'url')
+        self.username = xrc.XRCCTRL(self, 'username')
+        self.password = xrc.XRCCTRL(self, 'password')
+        self.verify = xrc.XRCCTRL(self, 'verify')
         self.set_service()
 
     def set_service(self):
@@ -85,18 +85,17 @@ class Prefs(wx.Dialog):
 class Upload(wx.Dialog):
     def __init__(self, file, dl, params):
         self.xrc = xrc.XmlResource('upload.xrc')
-        self.upload = self.xrc.LoadDialog(None, 'upload')
-        self.file = xrc.XRCCTRL(self.upload, 'file')
-        self.gauge = xrc.XRCCTRL(self.upload, 'progress')
-        self.status = xrc.XRCCTRL(self.upload, 'status')
-        self.action = xrc.XRCCTRL(self.upload, 'action')
-        self.PostCreate(self.upload)
+        self.PostCreate(self.xrc.LoadDialog(None, 'upload'))
         self.Bind(wx.EVT_CLOSE, self.on_cancel)
-        self.request = dl.new_ticket(file, TicketParams(), async=True,
+        self.file = xrc.XRCCTRL(self, 'file')
+        self.gauge = xrc.XRCCTRL(self, 'progress')
+        self.status = xrc.XRCCTRL(self, 'status')
+        self.request = dl.new_ticket(file, params, async=True,
                                      complete_fn=self.completed,
                                      failed_fn=self.failed,
                                      progress_fn=self.progress)
         self.file.SetLabel(os.path.basename(file))
+        self.action = xrc.XRCCTRL(self, 'action')
         self.action.SetLabel("Cancel")
         self.action.Bind(wx.EVT_BUTTON, self.on_cancel)
         self.Fit()
@@ -153,6 +152,64 @@ class Upload(wx.Dialog):
         self.Destroy()
 
 
+class NewTicket(wx.Dialog):
+    def __init__(self, dl, ticket_params, change_fn):
+        self.dl = dl
+        self.def_ticket_params = ticket_params
+        self.change_fn = change_fn
+        self.xrc = xrc.XmlResource('newticket.xrc')
+        self.PostCreate(self.xrc.LoadDialog(None, 'newticket'))
+        self.Bind(wx.EVT_CLOSE, self.on_close)
+        self.file = xrc.XRCCTRL(self, 'file')
+        self.perm = xrc.XRCCTRL(self, 'perm')
+        self.perm.Bind(wx.EVT_CHECKBOX, self.on_perm)
+        self.total_days = xrc.XRCCTRL(self, 'total_days')
+        self.hours_after_dl = xrc.XRCCTRL(self, 'hours_after_dl')
+        self.downloads = xrc.XRCCTRL(self, 'downloads')
+        self.upload = xrc.XRCCTRL(self, 'upload')
+        self.upload.Bind(wx.EVT_BUTTON, self.on_upload)
+        self.set_defaults = xrc.XRCCTRL(self, 'set_defaults')
+        self.set_defaults.Bind(wx.EVT_BUTTON, self.on_set_defaults)
+        self.ticket_params = copy.copy(self.def_ticket_params)
+        self.set_ticket_params(self.ticket_params)
+        self.Show()
+
+    def on_perm(self, evt=None):
+        enable = not self.perm.GetValue()
+        self.total_days.Enable(enable)
+        self.hours_after_dl.Enable(enable)
+        self.downloads.Enable(enable)
+
+    def set_ticket_params(self, ticket_params):
+        self.perm.SetValue(ticket_params.perm)
+        self.total_days.SetValue(ticket_params.total_days)
+        self.hours_after_dl.SetValue(ticket_params.hours_after_dl)
+        self.downloads.SetValue(ticket_params.downloads)
+        self.on_perm()
+
+    def get_ticket_params(self, ticket_params):
+        ticket_params.perm = self.perm.GetValue()
+        ticket_params.total_days = self.total_days.GetValue()
+        ticket_params.hours_after_dl = self.hours_after_dl.GetValue()
+        ticket_params.downloads = self.downloads.GetValue()
+
+    def on_set_defaults(self, evt):
+        self.get_ticket_params(self.def_ticket_params)
+        self.change_fn()
+
+    def on_upload(self, evt):
+        path = self.file.GetPath().encode('utf8')
+        if not len(path):
+            wx.MessageBox('Please select a file!', 'New Ticket', wx.OK | wx.ICON_ERROR)
+        else:
+            self.get_ticket_params(self.ticket_params)
+            Upload(path, self.dl, self.ticket_params)
+            self.on_close()
+
+    def on_close(self, evt=None):
+        self.Destroy()
+
+
 class DLApp(wx.App):
     def OnInit(self):
         self.dl = DL()
@@ -171,30 +228,38 @@ class DLApp(wx.App):
     def load_prefs(self):
         cfgpath = os.path.expanduser(DL_RC)
         self.cfg = configobj.ConfigObj(cfgpath)
-
         v = validate.Validator()
+
         self.dl.service.url = v.check('string', self.cfg.get('url', ''))
         self.dl.service.username = v.check('string', self.cfg.get('user', ''))
         self.dl.service.password = v.check('string', self.cfg.get('pass', ''))
         self.dl.service.verify = v.check('boolean', self.cfg.get('verify', True))
         self.dl.service.agent = DL_AGENT
 
+        self.ticket_params = TicketParams()
+        self.ticket_params.perm = v.check('boolean', self.cfg.get('perm', False))
+        self.ticket_params.total_days = v.check('integer', self.cfg.get('total_days', 7))
+        self.ticket_params.hours_after_dl = v.check('integer', self.cfg.get('hours_after_dl', 24))
+        self.ticket_params.downloads = v.check('integer', self.cfg.get('downloads', 0))
+
     def save_prefs(self):
-        self.cfg['url'] = self.dl.service.url;
-        self.cfg['user'] = self.dl.service.username;
-        self.cfg['pass'] = self.dl.service.password;
-        self.cfg['verify'] = self.dl.service.verify;
+        self.cfg['url'] = self.dl.service.url
+        self.cfg['user'] = self.dl.service.username
+        self.cfg['pass'] = self.dl.service.password
+        self.cfg['verify'] = self.dl.service.verify
+        self.cfg['perm'] = self.ticket_params.perm
+        self.cfg['total_days'] = self.ticket_params.total_days
+        self.cfg['hours_after_dl'] = self.ticket_params.hours_after_dl
+        self.cfg['downloads'] = self.ticket_params.downloads
         self.cfg.write()
 
     def express_ticket(self, evt=None):
-        path = wx.FileSelector(flags=wx.FILE_MUST_EXIST)
+        path = wx.FileSelector(flags=wx.FILE_MUST_EXIST).encode('utf8')
         if len(path):
-            path = path.encode('utf8')
-            Upload(path, self.dl, TicketParams())
+            Upload(path, self.dl, self.ticket_params)
 
     def new_ticket(self, evt=None):
-        # TODO: this should prompt for ticket parameters
-        self.express_ticket()
+        NewTicket(self.dl, self.ticket_params, self.save_prefs)
 
     def show_prefs(self, evt=None):
         self.prefs.Show()
