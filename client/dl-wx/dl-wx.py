4dcf1ede050b4cc858133d6a5a5ed51fff77e9ab
Yuri D'Elia <wavexx@thregr.org>
2017-09-02 16:54:12 +0200

    dl-wx: group preferences in the same object

--- client/dl-wx/dl-wx.py
+++ client/dl-wx/dl-wx.py
@@ -51,9 +51,16 @@ class TaskBarIcon(wx.TaskBarIcon):
         return menu
 
 
-class Prefs(wx.Dialog):
-    def __init__(self, service, change_fn):
-        self.service = service
+class Preferences(object):
+    def __init__(self, service=None, ticket_params=None, grant_params=None):
+        self.service = Service() if service is None else service
+        self.ticket_params = TicketParams() if ticket_params is None else ticket_params
+        self.grant_params = GrantParams() if grant_params is None else grant_params
+
+
+class PrefDialog(wx.Dialog):
+    def __init__(self, prefs, change_fn):
+        self.prefs = prefs
         self.change_fn = change_fn
         self.xrc = xrc.XmlResource(os.path.join(RC_PATH, 'preferences.xrc'))
         self.PostCreate(self.xrc.LoadDialog(None, 'preferences'))
@@ -66,7 +73,7 @@ class Prefs(wx.Dialog):
         self.save = xrc.XRCCTRL(self, 'save')
         self.save.Bind(wx.EVT_BUTTON, self.on_save)
         self.Bind(wx.EVT_SHOW, self.on_show)
-        if service.url:
+        if prefs.service.url:
             self.cancel.Show()
             self.Bind(wx.EVT_CLOSE, self.on_close)
         else:
@@ -74,37 +81,37 @@ class Prefs(wx.Dialog):
             self.Bind(wx.EVT_CLOSE, self.on_save)
 
     def on_show(self, evt):
-        self.set_service()
+        self.set_prefs()
 
-    def set_service(self):
-        self.url.SetValue(self.service.url)
-        self.username.SetValue(self.service.username)
-        self.password.SetValue(self.service.password)
-        self.verify.SetValue(self.service.verify)
+    def set_prefs(self):
+        self.url.SetValue(self.prefs.service.url)
+        self.username.SetValue(self.prefs.service.username)
+        self.password.SetValue(self.prefs.service.password)
+        self.verify.SetValue(self.prefs.service.verify)
 
-    def get_service(self, service):
-        service.url = self.url.GetValue().encode('utf8')
-        service.username = self.username.GetValue().encode('utf8')
-        service.password = self.password.GetValue().encode('utf8')
-        service.verify = self.verify.GetValue()
+    def get_prefs(self, prefs):
+        prefs.service.url = self.url.GetValue().encode('utf8')
+        prefs.service.username = self.username.GetValue().encode('utf8')
+        prefs.service.password = self.password.GetValue().encode('utf8')
+        prefs.service.verify = self.verify.GetValue()
 
     def on_close(self, evt=None):
         self.Hide()
 
     def on_save(self, evt):
-        service = Service()
-        self.get_service(service)
+        prefs = Preferences()
+        self.get_prefs(prefs)
         error = None
-        if not service.url:
+        if not prefs.service.url:
             error = "The REST URL is mandatory"
-        elif not service.username:
+        elif not prefs.service.username:
             error = "The username is mandatory"
-        elif not service.password:
+        elif not prefs.service.password:
             error = "The password is mandatory"
         if error is not None:
             wx.MessageBox(error, 'Preferences', wx.OK | wx.ICON_ERROR)
         else:
-            self.get_service(self.service)
+            self.get_prefs(self.prefs)
             self.change_fn()
             self.on_close()
 
@@ -197,9 +204,9 @@ class Upload(wx.Dialog):
 
 
 class NewTicket(wx.Dialog):
-    def __init__(self, dl, ticket_params, change_fn):
+    def __init__(self, dl, prefs, change_fn):
         self.dl = dl
-        self.def_ticket_params = ticket_params
+        self.prefs = prefs
         self.change_fn = change_fn
         self.xrc = xrc.XmlResource(os.path.join(RC_PATH, 'newticket.xrc'))
         self.PostCreate(self.xrc.LoadDialog(None, 'newticket'))
@@ -215,7 +222,7 @@ class NewTicket(wx.Dialog):
         self.upload.Bind(wx.EVT_BUTTON, self.on_upload)
         self.set_defaults = xrc.XRCCTRL(self, 'set_defaults')
         self.set_defaults.Bind(wx.EVT_BUTTON, self.on_set_defaults)
-        self.ticket_params = copy.copy(self.def_ticket_params)
+        self.ticket_params = copy.copy(self.prefs.ticket_params)
         self.set_ticket_params(self.ticket_params)
 
     def on_perm(self, evt=None):
@@ -238,7 +245,7 @@ class NewTicket(wx.Dialog):
         ticket_params.downloads = self.downloads.GetValue()
 
     def on_set_defaults(self, evt):
-        self.get_ticket_params(self.def_ticket_params)
+        self.get_ticket_params(self.prefs.ticket_params)
         self.change_fn()
 
     def on_upload(self, evt):
@@ -264,16 +271,16 @@ class DLApp(wx.App):
                           'dl-wx', wx.OK | wx.ICON_ERROR)
             return False
 
-        self.dl = DL()
         self.load_prefs()
-        self.prefs = Prefs(self.dl.service, self.save_prefs)
-        if not self.dl.service.url:
+        self.pd = PrefDialog(self.prefs, self.save_prefs)
+        if not self.prefs.service.url:
             wx.MessageBox('This is the first time you run ' + DL_DESCRIPTION +
                           '. You need to configure it first.',
                           'Preferences', wx.OK | wx.ICON_INFORMATION)
-            self.prefs.ShowModal()
+            self.pd.ShowModal()
 
-        self.nt = NewTicket(self.dl, self.ticket_params, self.save_prefs)
+        self.dl = DL(self.prefs.service)
+        self.nt = NewTicket(self.dl, self.prefs, self.save_prefs)
         self.tbi = TaskBarIcon(self)
         stub = wx.Frame(None)
         menu = wx.MenuBar()
@@ -287,39 +294,40 @@ class DLApp(wx.App):
         self.cfg = configobj.ConfigObj(cfgpath)
         v = validate.Validator()
 
-        self.dl.service.url = v.check('string', self.cfg.get('url', ''))
-        self.dl.service.username = v.check('string', self.cfg.get('user', ''))
-        self.dl.service.password = v.check('string', self.cfg.get('pass', ''))
-        self.dl.service.verify = v.check('boolean', self.cfg.get('verify', True))
-        self.dl.service.agent = DL_AGENT
+        self.prefs = Preferences()
 
-        self.ticket_params = TicketParams()
-        self.ticket_params.permanent = v.check('boolean', self.cfg.get('perm', False))
-        self.ticket_params.total = v.check('integer', self.cfg.get('total_days', 365)) * 3600 * 24
-        self.ticket_params.lastdl = v.check('integer', self.cfg.get('days_after_dl', 30)) * 3600 * 24
-        self.ticket_params.downloads = v.check('integer', self.cfg.get('downloads', 0))
+        self.prefs.service.url = v.check('string', self.cfg.get('url', ''))
+        self.prefs.service.username = v.check('string', self.cfg.get('user', ''))
+        self.prefs.service.password = v.check('string', self.cfg.get('pass', ''))
+        self.prefs.service.verify = v.check('boolean', self.cfg.get('verify', True))
+        self.prefs.service.agent = DL_AGENT
+
+        self.prefs.ticket_params.permanent = v.check('boolean', self.cfg.get('perm', False))
+        self.prefs.ticket_params.total = v.check('integer', self.cfg.get('total_days', 365)) * 3600 * 24
+        self.prefs.ticket_params.lastdl = v.check('integer', self.cfg.get('days_after_dl', 30)) * 3600 * 24
+        self.prefs.ticket_params.downloads = v.check('integer', self.cfg.get('downloads', 0))
 
     def save_prefs(self):
-        self.cfg['url'] = self.dl.service.url
-        self.cfg['user'] = self.dl.service.username
-        self.cfg['pass'] = self.dl.service.password
-        self.cfg['verify'] = self.dl.service.verify
-        self.cfg['perm'] = self.ticket_params.permanent
-        self.cfg['total_days'] = self.ticket_params.total / (3600 * 24)
-        self.cfg['days_after_dl'] = self.ticket_params.lastdl / (3600 * 24)
-        self.cfg['downloads'] = self.ticket_params.downloads
+        self.cfg['url'] = self.prefs.service.url
+        self.cfg['user'] = self.prefs.service.username
+        self.cfg['pass'] = self.prefs.service.password
+        self.cfg['verify'] = self.prefs.service.verify
+        self.cfg['perm'] = self.prefs.ticket_params.permanent
+        self.cfg['total_days'] = self.prefs.ticket_params.total / (3600 * 24)
+        self.cfg['days_after_dl'] = self.prefs.ticket_params.lastdl / (3600 * 24)
+        self.cfg['downloads'] = self.prefs.ticket_params.downloads
         self.cfg.write()
 
     def express_ticket(self, evt=None):
         path = wx.FileSelector(flags=wx.FD_OPEN|wx.FD_FILE_MUST_EXIST).encode('utf8')
         if path:
-            Upload(path, self.dl, self.ticket_params)
+            Upload(path, self.dl, self.prefs.ticket_params)
 
     def new_ticket(self, evt=None):
         self.nt.Show()
 
     def show_prefs(self, evt=None):
-        self.prefs.Show()
+        self.pd.Show()
 
     def show_about(self, evt=None):
         wx.MessageBox(DL_DESCRIPTION + " " + DL_AGENT + "\n" + DL_URL,
@@ -329,7 +337,7 @@ class DLApp(wx.App):
         self.ExitMainLoop()
 
     def MacOpenFile(self, path):
-        Upload(path.encode('utf8'), self.dl, self.ticket_params)
+        Upload(path.encode('utf8'), self.dl, self.prefs.ticket_params)
 
     def MacReopenApp(self):
         self.express_ticket()
