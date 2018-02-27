3b45ec292c0d9c05b499b448165871a114d87236
Yuri D'Elia <wavexx@thregr.org>
2018-02-27 12:08:14 +0100

    Fix returnBytes in PHP 7+
    PHP 7 is (finally?) a bit more picky when converting strings to integers.
    Chop the suffix before trying to multiply the value.

--- htdocs/include/basefuncs.php
+++ htdocs/include/basefuncs.php
@@ -3,8 +3,17 @@
 
 function returnBytes($val)
 {
+  if(!strlen($val)) return 0;
+  if(is_int($val)) return $val;
+
+  // parse suffix (if any)
   $val = trim($val);
   $last = strtolower($val{strlen($val)-1});
+  if($last === (string)(int)$last || $last === ".")
+    return $val; // no sfx
+
+  // handle mulitiplier
+  $val = substr($val, 0, -1);
   switch($last)
   {
   case 'g': $val *= 1024;
